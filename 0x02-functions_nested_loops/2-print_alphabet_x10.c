30cd201ebff1cf766ad7450a46922e3136a51aa3
Quinton <Qrikhotso@yahoo.com>
2022-10-18 11:52:51 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int num;
+
+	num = 0;
+
+	while (num <= 9)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_purchar(c);
+		}
+		_putchar('\n');
+		num++;
+	}
+}
