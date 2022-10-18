b89a8aab281200ca459fa5f461b01f10b203d8a6
Quinton <Qrikhotso@yahoo.com>
2022-10-18 11:13:37 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Prints the "_putchar"
@@ -9,6 +9,14 @@
 int main(void)
 {
 	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('/n');
 
 	return (0);
 }
