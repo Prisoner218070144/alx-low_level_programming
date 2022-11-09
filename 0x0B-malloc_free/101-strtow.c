f8745683f03cdc5a5975bee80e271f3462ba0eb9
Quinton <Qrikhotso@yahoo.com>
2022-11-09 03:50:30 +0200

    6. I will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * strtow - splits a string into words
+  * @str: the string to be split
+  *
+  * Return: NULL if str == NULL or str == ""
+  */
+char **strtow(char *str)
+{
+	
+}
