3087fc7fa14fe2b1baa45a8f8152b3dde7cd0da4
Quinton <Qrikhotso@yahoo.com>
2022-11-09 03:24:05 +0200

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-  * alloc_grid - ...
-  * @width: ...
-  * @height: ...
+  * alloc_grid -  2 dimensional array of integers
+  * @width: width of the grid
+  * @height: height of the grid
   *
-  * Return: ...
+  * Return: NULL
   */
 int **alloc_grid(int width, int height)
 {
