7a4fbeb56186817ac97cc267f76cc79269cc0f74
Prisoner218070144 <Qrikhotso@yahoo.com>
2022-11-25 07:53:56 +0200

    trial and error

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * print_list - Prints elements of a list
+  * @h: a linked list
+  *
+  * Return: the nodes
+  */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
